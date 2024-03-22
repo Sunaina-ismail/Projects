2da8756e59d092b7ce5cd9476312944f8177a4d2
Sunaina-ismail <164198987+Sunaina-ismail@users.noreply.github.com>
2024-03-22 03:44:48 -0700

    simple-calculator

---
+++ naina6-simple-calculator/index.ts
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {
+     message: "Enter firstNumber",
+     type: "number",
+     name:"firstNumber",
+ },
+
+ {
+
+    message: "Enter secondNumber",
+    type: "number",
+    name:"secondNumber",
+
+ },
+
+ {
+    message: "select one of the operators to perform Operation",
+    type: "list",
+    name:"operator",
+    choices: ["Addition", "Subtraction","Multiplication","Division"],
+},
+])
+
+
+
+if(answer.operator === "Addition"){
+console.log("The Sum of your number is: ", answer.firstNumber + answer.secondNumber);
+}else if(answer.operator === "Subtraction" ){
+    console.log("The Difference of your number is:", answer.firstNumber - answer.secondNumber);
+}else if(answer.operator === "Multiplication" ){
+console.log("The Difference of your number is:", answer.firstNumber * answer.secondNumber)
+}else if(answer.operator === "Division"){
+    console.log("the Quotient of your number is: ", answer.firstNumber / answer.secondNumber);
+}else{
+    console.log("Please Select valid Operator");
+    
+}
+
+
+
+
+
