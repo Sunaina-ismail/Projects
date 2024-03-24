ff842db9991487be136bda9d3b95b99bc4007407
Sunaina-ismail <164198987+Sunaina-ismail@users.noreply.github.com>
2024-03-23 19:53:28 -0700

    number guessing game

---
+++ naina6-number-guessing-game/index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);   
+
+const answer = await inquirer.prompt([
+  {
+    name: "userguessednumber",
+    type: "number",
+    message: "Please guess a number from 1 to 10 =",
+  },
+]);
+
+if (answer.userguessednumber === randomNumber) {
+  console.log("congratulation ! You guessed the right number. ");
+} else {
+  console.log("Sorry! You guessed the wrong number.");
+}
